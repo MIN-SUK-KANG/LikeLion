05b80265d42c815f434fd9645390da470db62538
Minsuk Kang <str2an5g9er1@naver.com>
2025-02-21 15:56:25 +0900

    char, string, input, output

---
+++ LikeLion5/LikeLion5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //다른 학생들이 정수 데이터, 부동소수점 데이터, 부호 없는 데이터, 숫자 리터럴 접미사를 배우는 내용
+            //다른 학생들이 논리 연산자, char, string, 상수를 배우는 내용
+
+            /*
+             * 나한테도 중요한 내용!!
+             * 
+             * char는 기본 정의 데이터 형식이다보니, 가감승제 등의 수학적 연산자를 사용할 시 작신이 저장된 Ascii 위치의 십진값으로 '연산'이 된다!!
+             * 하지만 string은 C, C++, C#의 언어 구성 레벨에서 정의된 '클래스'기 때문에, 수학적 연산자들이 오버로딩 되어있어서 +하면 문자열이 이어지는 것!
+             * 
+             */
+        }
+    }
+}
---
+++ StudyReadLine/StudyReadLine/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("루인 스킬 피해량은 얼마입니까? (%값 입력, 숫자만) : ");
+            float maxDamage = float.Parse(Console.ReadLine());
+            Console.Write("카드 게이지 획득량은 얼마입니까? (%값 입력, 숫자만) : ");
+            float maxCard = float.Parse(Console.ReadLine());
+            Console.Write("각성기 피해는 얼마입니까? (%값 입력, 숫자만) : ");
+            float maxTrans = float.Parse(Console.ReadLine());
+            Console.Write("현재 플레이어의 최대 마나는 얼마입니까? : ");
+            int maxMP = int.Parse(Console.ReadLine());
+            Console.Write("전투 중 마나 회복량은 얼마입니까? : ");
+            int reMPbattle = int.Parse(Console.ReadLine());
+            Console.Write("비전투 중 마나 회복량은 얼마입니까? : ");
+            int reMPday = int.Parse(Console.ReadLine());
+            Console.Write("이동속도는 얼마입니까? (%값 입력, 숫자만) : ");
+            float moveSpd = float.Parse(Console.ReadLine());
+            Console.Write("탈 것 속도는 얼마입니까? (%값 입력, 숫자만) : ");
+            float rideSpd = float.Parse(Console.ReadLine());
+            Console.Write("운반 속도는 얼마입니까? (%값 입력, 숫자만) : ");
+            float takeSpd = float.Parse(Console.ReadLine());
+            Console.Write("최종 스킬 재사용 대기 시간 감소 효과는 얼마입니까? (%값 입력, 숫자만) : ");
+            float coolDown = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n\n==활동=======================▼==");
+            Console.WriteLine($"루인 스킬 피해               {maxDamage}%");
+            Console.WriteLine($"카드 게이지 획득량           {maxCard}%");
+            Console.WriteLine($"각성기 피해                  {maxTrans}%");
+            Console.WriteLine($"최대 마나                    {maxMP}");
+            Console.WriteLine($"전투 중 마나 회복량           {reMPbattle}");
+            Console.WriteLine($"비전투 중 마나 회복량         {reMPday}");
+            Console.WriteLine($"이동 속도                  {moveSpd}%");
+            Console.WriteLine($"탈 것 속도                 {rideSpd:F1}%");
+            Console.WriteLine($"운반 속도                  {takeSpd:F1}%");
+            Console.WriteLine($"스킬 재사용 대기 시간 감소   {coolDown}%");
+
+
+        }
+    }
+}
