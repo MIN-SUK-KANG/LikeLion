e9dc001774ef9543b597a83b993f121ea1015fff
Minsuk Kang <str2an5g9er1@naver.com>
2025-02-21 11:33:41 +0900

    print tests

---
+++ LikeLion4/LikeLion4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string greeting;
+            //greeting = "Hello, World!";
+
+            //Console.WriteLine(greeting);
+
+
+
+            //int score = 100;
+            //double temperature = 36.5;
+            //string city = "Seoul";
+
+            //Console.WriteLine(score);
+            //Console.WriteLine(temperature);
+            //Console.WriteLine(city);
+
+
+
+            //int x = 10, y = 20, z = 30;
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+
+
+
+            //const double pi = 3.14159;
+            //const int MaxScore = 100;
+
+            //Console.WriteLine("Pi : " + pi);
+            //Console.WriteLine("Max Score : " + MaxScore);
+
+
+        }
+    }
+}
