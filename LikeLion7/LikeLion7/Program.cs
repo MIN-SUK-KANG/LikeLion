5765936096099e1440dd94002efbb123b20f6402
Minsuk Kang <str2an5g9er1@naver.com>
2025-02-24 16:42:56 +0900

    0224

---
+++ LikeLion7/LikeLion7/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //단항 연산자
+            //int number = 5;
+            //bool flag = true;
+
+
+            //Console.WriteLine(+number); //양수 출력 : 5
+            //Console.WriteLine(-number); //음수 출력 : -5
+
+            //Console.WriteLine(!flag);  //논리 부정 : false
+
+            //int num = 10;
+            //int result = ~num;   //모든 비트 반전 : 1111 0101 결과
+
+
+            //Console.WriteLine("원래 값 : " + num);
+            //Console.WriteLine("~ 연산자 적용 후: " + result);
+
+            //int iKor = 90;
+            //int iEng = 75;
+            //int iMath = 58;
+
+            //int sum = 0;
+            //float average = 0.0f;
+
+            //sum = iKor + iEng + iMath;
+
+            //average = (float)sum / 3;  //평균
+
+            //Console.WriteLine("총점 : " + sum);
+            //Console.WriteLine("평균 : " + average);
+
+            //int a = 10, b = 3;
+
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a - b);
+            //Console.WriteLine(a * b);
+            //Console.WriteLine(a / b);
+            //Console.WriteLine(a % b);
+
+            //string firstName = "Alice";
+            //string lastName = "Smith";
+
+            //Console.WriteLine(firstName + " " + lastName); //출력
+
+            //int a = 10;
+            //a += 5;  // a  = a + 5;
+            //Console.WriteLine(a);
+            //a -= 5;  // a  = a - 5;
+            //Console.WriteLine(a);
+            //a *= 5;  // a  = a * 5;
+            //Console.WriteLine(a);
+            //a /= 5;  // a  = a / 5;
+            //Console.WriteLine(a);
+            //a %= 5;  // a  = a % 5;
+            //Console.WriteLine(a);
+
+
+            //-----소수점 포맷 출력 예시
+            //double value = 1234.56;
+            //Console.WriteLine(value.ToString("F2")); // 출력: "1234.56"
+
+
+            //문제 1.학점 평균 계산 프로그램
+            //설명:
+            //국어, 영어, 수학 점수를 사용자로부터 입력받아 총점과 평균을 구하는 프로그램을 작성하세요.
+            //요구사항:
+
+            //각 과목의 점수는 정수형으로 입력받습니다.
+            //총점을 구한 후, 평균을 계산할 때 정수형 총점을 실수형으로 캐스팅하여 소수점까지 정확하게 계산합니다.
+            //평균은 소수점 둘째 자리까지 출력하세요.
+
+            Console.Write("국어 점수를 입력: ");
+            int krtest = int.Parse(Console.ReadLine());
+            Console.Write("영어 점수를 입력: ");
+            int entest = int.Parse(Console.ReadLine());
+            Console.Write("수학 점수를 입력: ");
+            int mathtest = int.Parse(Console.ReadLine());
+
+
+            int total = krtest + entest + mathtest;
+            double average = (double)total / 3;
+
+            Console.Write($"총점: {total}");
+            Console.Write($"평균: {average.ToString("F2")}");
+
+
+            //문제 2.비트 반전(~) 연산자 활용 프로그램
+            //설명:
+            //사용자로부터 정수를 입력받아, 해당 정수의 모든 비트를 반전(~)한 결과를 출력하는 프로그램을 작성하세요.
+            //요구사항:
+
+            //정수를 입력받습니다.
+            //비트 반전 연산자(~)를 이용하여 입력된 정수의 비트를 반전합니다.
+            //원래의 값과 비트 반전 후의 값을 함께 출력합니다.
+
+            Console.Write("\n\n\n정수를 입력: ");
+            int given = int.Parse(Console.ReadLine());
+            int flipped = ~given;
+
+            Console.Write($"원래 값: {given}");
+            Console.Write($"비트 반전 값: {flipped}");
+
+
+
+            //int b = 3;
+
+            //전위 ++b , 후위 b++
+            //Console.WriteLine("b의 값은 : " + (b++));
+
+            //전위연산은 '연산 이후 해당 라인 처리', 후위 연산은 '해당 라인 처리 이후 연산'
+            //즉 Console.WriteLine 내부에 ++b를 넣을 경우 'b를 ++ 연산한 이후 출력'이 되고, b++를 넣을 경우 '출력 이후 b를 ++ 연산'이 된다.
+
+
+            //int x = 5, y = 10;
+
+            //Console.WriteLine(x == y);  // false
+            //Console.WriteLine(x != y);  // true
+            //Console.WriteLine(x < y);   // true
+            //Console.WriteLine(x > y);   // false
+            //Console.WriteLine(x >= y);  // false
+            //Console.WriteLine(x <= y);  //true  
+
+            //bool a = false;
+            //bool b = false;
+            //Not
+            //!a
+
+            //b = !a;
+
+            //Console.WriteLine(b); //true 
+        }
+    }
+}
---
+++ LikeLion8/LikeLion8/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //비트연산자
+            //int x = 5; // 0101
+            //int y = 3; // 0011 
+
+            //Console.WriteLine(x & y); //AND : 1 (0001)
+            //Console.WriteLine(x | y); //OR : 7 (0111)
+            //Console.WriteLine(x ^ y); //XOR :6   (0110)
+            //Console.WriteLine(~x);    //NOT : -6 
+
+            //int value = 4;  //0100
+
+            //Console.WriteLine(value << 1); //왼쪽이동 : 8 (1000)
+            //Console.WriteLine(value >> 1); //오른쪽이동: 2 (0010)
+
+            //int a = 10, b = 20;
+
+            //int max;
+
+            //max = (a < b) ? a : b; //삼항 연산자
+
+            //Console.WriteLine(max);
+
+            //int key = 1;
+
+            //string str;
+            //str = (key == 2) ? "문이열렸습니다." : "문을 못열었습니다.";
+
+            //Console.WriteLine(str);
+
+            //int result = 10 + 2 * 5; //곱셈이 덧셈보다 우선
+            //Console.WriteLine(result);
+
+            //int adjustedResult = (10 + 2) * 5; //괄호 우선순위 먼저계산
+
+            //Console.WriteLine(adjustedResult);
+
+            //int score = 85;
+
+            //if (score >= 90)
+            //{
+            //    Console.WriteLine("A 학점");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("B 학점");
+            //}
+
+            //string GameID = "멋사검존";
+
+
+            //if (GameID == "멋사검존")
+            //{
+            //    Console.WriteLine("아이디가 일치합니다.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("아이디가 일치하지 않습니다.");
+            //}
+
+            //int score = 60;
+
+            //if (score >= 90)
+            //{
+            //    Console.WriteLine("A 학점");
+            //}
+            //else if (score >= 80)
+            //{
+            //    Console.WriteLine("B 학점");
+            //}
+            //else if (score >= 70)
+            //{
+            //    Console.WriteLine("C 학점");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("F 학점");
+            //}
+
+
+
+            //1단계
+            //가지고있는 소지금을 입력하세요 :  
+            //0~100  무한의대검 +1
+            //101~200 카타나 +2
+            //201~300 진은검 +3
+            //301~400 집판검 +4
+            //401~500 엑스칼리버 +5
+            //501~600 유령검 +6
+            //601~ 전설의검 +7
+
+            //2단계
+            //캐릭터 이름
+            //공격력 : 100 + 1 
+
+            Console.WriteLine("가지고있는 소지금을 입력하세요: ");
+            int money = int.Parse(Console.ReadLine());
+            Console.WriteLine("캐릭터의 이름을 입력하세요: ");
+            string name = Console.ReadLine();
+            int power = 100;
+            string weapon = "";
+            if(money >= 0 && money <= 100)
+            {
+                power = power + 1;
+                weapon = "무한의대검";
+            } else if(money > 100 && money <= 200)
+            {
+                power = power + 2;
+                weapon = "카타나";
+            }
+            else if (money > 200 && money <= 300)
+            {
+                power = power + 3;
+                weapon = "진은검";
+            }
+            else if (money > 300 && money <= 400)
+            {
+                power = power + 4;
+                weapon = "집판검";
+            }
+            else if (money > 400 && money <= 500)
+            {
+                power = power + 5;
+                weapon = "엑스칼리버";
+            }
+            else if (money > 500 && money <= 600)
+            {
+                power = power + 6;
+                weapon = "유령검";
+            }
+            else if (money < 0)
+            {
+                Console.WriteLine("돈이 없어...");
+            }
+            else
+            {
+                power = power + 7;
+                weapon = "전설의검";
+            }
+            Console.WriteLine($"{weapon}을 획득한 {name}의 공격력은...");
+            Console.WriteLine($"무려 {power}점에 다다랐다!");
+
+
+        }
+    }
+}
