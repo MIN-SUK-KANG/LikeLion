faf15e19d7998821b9cddd7f02488e84faa9131d
Minsuk Kang <str2an5g9er1@naver.com>
2025-03-14 15:48:44 +0900

    1945 이어서...

--- Unity/1945Game/Assets/Script/Monster.cs
+++ Unity/1945Game/Assets/Script/Monster.cs
@@ -2,15 +2,52 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float Speed = 3;
+    public float Delay = 1f;
+    public Transform ms1;
+    public Transform ms2;
+    public GameObject bullet;
+    //아이템 가져오기
+    public GameObject Item = null;
+
     void Start()
     {
-        
+        //한번함수호출
+        Invoke("CreateBullet", Delay);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        //아래 방향으로 움직여라
+        transform.Translate(Vector3.down * Speed * Time.deltaTime);
     }
+
+
+    void CreateBullet()
+    {
+        Instantiate(bullet, ms1.position, Quaternion.identity);
+        Instantiate(bullet, ms2.position, Quaternion.identity);
+
+        //재귀호출
+        Invoke("CreateBullet", Delay);
+    }
+    public void ItemDrop()
+    {
+        //아이템 생성
+        Instantiate(Item, transform.position, Quaternion.identity);
+    }
+
+
+
+    //미사일에 따른 데미지 입는 함수
+    public void Damage(int attack)
+    {
+        ItemDrop();
+        Destroy(gameObject);
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+
 }
--- Unity/1945Game/Assets/Script/Player.cs
+++ Unity/1945Game/Assets/Script/Player.cs
@@ -7,6 +7,15 @@ public class Player : MonoBehaviour
 
     Animator ani; //애니메이터를 가져올 변수
 
+    public GameObject[] bullet;
+    public Transform pos = null;
+
+    public int power = 0;
+    [SerializeField]
+    private GameObject powerup;  //private 인스펙터에서 사용하는방법
+
+    //레이저
+
     void Start()
     {
         ani = GetComponent<Animator>();
@@ -42,11 +51,54 @@ public class Player : MonoBehaviour
         }
 
 
-
-
+        //스페이스
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //프리팹 위치 방향 넣고 생성
+            Instantiate(bullet[power], pos.position, Quaternion.identity);
+        }
 
         transform.Translate(moveX, moveY, 0);
 
 
+
+        /*
+        //캐릭터의 월드 좌표를 뷰포트 좌표계로 변환해준다. Wall 오브젝트 없이 직접 이동 범위 제한하기.
+
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
+        viewPos.x = Mathf.Clamp01(viewPos.x); //x값을 0이상, 1이하로 제한한다.
+        viewPos.y = Mathf.Clamp01(viewPos.y); //y값을 0이상, 1이하로 제한한다.
+
+        Vector3 worldPos = Camera.main.ViewportToWorldPoint(viewPos);//다시월드좌표로 변환
+        transform.position = worldPos; //좌표를 적용한다.
+
+        */
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Item"))
+        {
+            power += 1; //아이템 획득으로 파워업
+
+            if (power >= 3)
+                power = 3;
+            else
+            {
+                //파워업
+                GameObject go = Instantiate(powerup, transform.position, Quaternion.identity);
+                Destroy(go, 1);
+
+            }
+
+            //아이템 먹은 처리
+            Destroy(collision.gameObject);
+        }
+
+
+
+    }
+
+
+
 }
